5b062f64f3117f07a5f2a2ae4543970e048f5fa9
Hancer <hancermercedes@gmail.com>
2024-03-15 13:28:22 -0400

    adding the layers to the project and the entities of the db

---
+++ Entities/Contributor.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+
+public enum Status { Activo = 1, inactivo = 0 }
+public class Contributor
+{
+    public Guid ContributorId { get; set; }
+    public string? RncCedula { get; set; }
+    public string? Name { get; set; }
+    public Status Status { get; set; }
+}
